2559825e709dda2fb4aadb2d9e51fed5bccdeb37
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2024-04-04 14:26:37 -0700

    commit

---
+++ Input_Evidence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNet
+{
+    internal class Input_Evidence
+    {
+        public int value { get; set;}
+        public int weight { get; private set;}
+
+        public Input_Evidence(int value, int weight)
+        {
+            this.value = value;
+            this.weight = weight;
+        }
+
+    }
+}
