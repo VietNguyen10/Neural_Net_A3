4f3e72e528e7906e01e5c6bee38623a77c1dec7a
Haurence <79555142+qowier@users.noreply.github.com>
2024-04-07 22:15:30 -0700

    Adding cost options

---
+++ ICost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNet
+{
+    internal interface ICost
+    {
+        double CostFunction(double[] predictedOutputs, double[] expectedOutputs);
+
+        double CostDerivative(double predictedOutput, double expectedOutput);
+
+        Cost.CostType CostFunctionType();
+    }
+}
+}
